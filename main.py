254b661c192c4da8e97372ef88c44bd2d537fcee
knoxyys <93702638+knoxyys@users.noreply.github.com>
2025-05-14 23:56:40 +0000

    in the codespace strait up comitting it. and by it well lets just say. my code

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-Write Python code for both the Novel and Magazine classes modelled in the previous slide. Include a suitable constructor method which uses the Book constructor method. Instantiate 2 novels and 2 magazines and print their details.
-Create the Book class (plus methods and attributes)
-Create the Novel class that inherits from Book class.
-Create the Magazine class that inherits from Book class.
+class Book:
+    def __init__(self, title, author, published):
+        self.title = title
+        self.author = author
+        self.published = published
+
+
+
+
+# Write Python code for both the Novel and Magazine classes modelled in the previous slide. Include a suitable constructor method which uses the Book constructor method. Instantiate 2 novels and 2 magazines and print their details.
+# Create the Book class (plus methods and attributes)
+# Create the Novel class that inherits from Book class.
+# Create the Magazine class that inherits from Book class.
